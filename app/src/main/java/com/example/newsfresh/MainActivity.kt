166f4af0df0ad7a594801c0d073a5d5eaa9513c9
Ankit <ankitjaiswal885@gmail.com>
2021-09-13 22:09:01 +0530

    --added internet permissions --used volley library for api calling --fixed blank screen issues

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -4,32 +4,65 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.newsfresh.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mAdapter: NewsListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        val items = fetchData()
-        val adapter: NewsListAdapter = NewsListAdapter(items, this)
-        binding.recyclerView.adapter = adapter
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        binding.recyclerView.adapter = mAdapter
 
 
     }
 
-    private fun fetchData(): ArrayList<String>{
-        val list = ArrayList<String>()
-        for(i in 0 until 100){
-            list.add("Item: $i")
+    private fun fetchData(){
+       val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6da6d98b1f8745dd9e19bc12e077b275"
+//        val params = HashMap<String, String?>()
+//        params.put("user-agent","Mozilla/5.0")
+//        val jsonObject =  JSONObject(params as Map<String, String>)
+        val jsonObjectRequest = object: JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+
+                for(i in 0 until newsJsonArray.length() ){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage"),
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+            },
+            Response.ErrorListener {})
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["user-agent"] = "Mozilla/5.0"
+                return headers
+            }
         }
-        return list
+
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this, "item clicked is $item",Toast.LENGTH_LONG).show()
+    override fun onItemClicked(item: News) {
+
     }
 }
