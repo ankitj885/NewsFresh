d4ce272afea79919563b293a6c3d6adacc11a098
Ankit <ankitjaiswal885@gmail.com>
2021-09-13 23:59:29 +0530

    --added clicks handler --use of chrome custom tabs

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.newsfresh
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.Response
@@ -26,17 +28,18 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
 
     }
 
-    private fun fetchData(){
-       val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6da6d98b1f8745dd9e19bc12e077b275"
+    private fun fetchData() {
+        val url =
+            "https://newsapi.org/v2/top-headlines?country=in&apiKey=6da6d98b1f8745dd9e19bc12e077b275"
 //        val params = HashMap<String, String?>()
 //        params.put("user-agent","Mozilla/5.0")
 //        val jsonObject =  JSONObject(params as Map<String, String>)
-        val jsonObjectRequest = object: JsonObjectRequest(Request.Method.GET, url, null,
+        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener {
                 val newsJsonArray = it.getJSONArray("articles")
                 val newsArray = ArrayList<News>()
 
-                for(i in 0 until newsJsonArray.length() ){
+                for (i in 0 until newsJsonArray.length()) {
                     val newsJsonObject = newsJsonArray.getJSONObject(i)
                     val news = News(
                         newsJsonObject.getString("title"),
@@ -50,6 +53,7 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
                 mAdapter.updateNews(newsArray)
             },
             Response.ErrorListener {})
+        //passing headers in api call
         {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
@@ -63,6 +67,8 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
     }
 
     override fun onItemClicked(item: News) {
-
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.url))
     }
 }
--- app/src/main/java/com/example/newsfresh/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsfresh/NewsListAdapter.kt
@@ -3,16 +3,20 @@ package com.example.newsfresh
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsfresh.databinding.ItemNewsBinding
 
-class NewsListAdapter(private val listener: NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+class NewsListAdapter(private val listener: NewsItemClicked) :
+    RecyclerView.Adapter<NewsViewHolder>() {
 
     private val items: ArrayList<News> = ArrayList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
         val viewHolder = NewsViewHolder(view)
-        view.setOnClickListener{
+        view.setOnClickListener {
             listener.onItemClicked(items[viewHolder.adapterPosition])
         }
         return viewHolder
@@ -21,13 +25,15 @@ class NewsListAdapter(private val listener: NewsItemClicked): RecyclerView.Adapt
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val currentItem = items[position]
         holder.titleView.text = currentItem.title
+        holder.authorView.text = currentItem.author
+        Glide.with(holder.itemView.context).load(currentItem.urlToImage).into(holder.imageView)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    fun updateNews(updatedNews: ArrayList<News>){
+    fun updateNews(updatedNews: ArrayList<News>) {
         items.clear()
         items.addAll(updatedNews)
 
@@ -35,10 +41,12 @@ class NewsListAdapter(private val listener: NewsItemClicked): RecyclerView.Adapt
     }
 }
 
-class NewsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val titleView: TextView = itemView.findViewById<TextView>(R.id.title)
+    val imageView: ImageView = itemView.findViewById<ImageView>(R.id.image)
+    val authorView: TextView = itemView.findViewById<TextView>(R.id.author)
 }
 
-interface NewsItemClicked{
+interface NewsItemClicked {
     fun onItemClicked(item: News)
 }
